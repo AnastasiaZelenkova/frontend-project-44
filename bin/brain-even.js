7301c332daad36a3b0bd2d11689b4201358e16f9
Anastasia Zelenkova <nastya.zelenkowa@yandex.ru>
2023-10-16 18:27:26 +0300

    update bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,13 +7,15 @@ const isEven = (n) => {
   return 'no';
 };
 
-const brainEven = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+const  getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const brainEven = (userName) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let i = 0;
   while (i < 3) {
-    const randomNum = Math.floor(Math.random() * (50 - 1)) + 1;
+    const randomNum = getRandomInt(1, 50);
     const correctAnswer = isEven(randomNum);
     console.log(`Question: ${randomNum}`);
     const userAnswer = readlineSync.question('Your answer: ');
@@ -21,11 +23,13 @@ const brainEven = () => {
       console.log('Correct!');
       i += 1;
     } else {
-      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
     }
   }
-  console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 console.log('Welcome to the Brain Games!');
-brainEven();
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+brainEven(name);
