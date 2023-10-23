915361fa407b1365ae1bfc5cdb49af4a107489b2
Anastasia Zelenkova <nastya.zelenkowa@yandex.ru>
2023-10-23 16:34:41 +0300

    add src/games/gcd-game.js and bin/brain-gcd.js

---
+++ bin/brain-gcd.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import brainGcd from '../src/games/gcd-game.js';
+import greeting from '../src/cli.js';
+
+const gameGcd = () => {
+    const name = greeting();
+    brainGcd(name);
+};
+
+gameGcd();
---
+++ src/games/gcd-game.js
@@ -0,0 +1,24 @@
+import { generalLogic, getRandomInt } from '../index.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+const minValue = 1;
+const maxValue = 50;
+
+const gcd = (expression) => {
+  const [a, b] = expression.split(' ');
+  if (a == 0) {
+    return b;
+  }
+  return gcd(`${b % a} ${a}`);
+};
+
+const getRandomGcd = (min, max) => {
+    const result = `${getRandomInt(min, max)} ${getRandomInt(min, max)}`;
+    return result;
+};
+
+const brainGcd = (userName) => {
+  generalLogic(userName, rules, getRandomGcd, gcd, minValue, maxValue);
+};
+
+export default brainGcd;
